f587d3b50dc7f68ddd07b6a942d5f848061cb41d
BadHope <badhope68@gmail.com>
2026-08-09 19:46:44 +0300

    docs: add readthedocs config

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+project = 'Sil Documentation'
+copyright = '2026 Sil Foundation'
+author = 'Sil Foundation'
+release = '1.0.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
