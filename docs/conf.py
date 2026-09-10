5d7e546ea6b26cdf2cd0e81f51b1965272915dec
BadHope <badhope68@gmail.com>
2026-09-11 01:01:23 +0300

    Initial commit

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+project = 'Sil Docs'
+copyright = '2026 Sil Foundation'
+author = 'Sil Foundation'
+release = '1.0.0'
+
+extensions = [
+    'sphinx_rtd_theme',
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+Sil Programming Language
+==========================
+
+There's nothing here yet, but updates are coming soon!
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
